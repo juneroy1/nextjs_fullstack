52e9c6e263d9b23847aab9ce9de202f68496da8e
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2022-10-16 21:28:32 +0800

    build: implement storybook

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+};
+// "stories": [
+//   "../stories/**/*.stories.mdx",
+//   "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+// ],
